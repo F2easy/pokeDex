d4f1da9971d7e1108ce412a6d689a2fb8a511f70
Felix-F <felixferreras2002@gmail.com>
2023-12-26 11:15:54 -0500

    error with the navigation to the login page get error

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -28,35 +28,34 @@ router.get('/signup', (req, res) => {
 
 // POST -> signup - /users/signup
 // this function will need to ve async, bc we need to use bcrypt
-router.post('/signup', async (req, res) =>{
-  const { username, loggedIn, userId } = req.session
+router.post('/signup', async (req, res) => {
+  // const { username, loggedIn, userId } = req.session
 
-  const newUser = req.body 
-  
-  res.send(newUser)
+  const newUser = req.body
 
   // we need to encrypt the password, which is what we will save to the db
   // bcrypt is an encryption service
   // genSalt creates 'salt rounds' -> puts it through 10 rounds of encrypting
-  // making the stored pw harder to hack/de-encrypt
-  newUser.password= await bcrypt.hash(
-    newUser.password, 
-    await bcrypt.genSalt(10)
+  // this makes the stored password harder to hack(de-encrypt)
+  newUser.password = await bcrypt.hash(
+      newUser.password, 
+      await bcrypt.genSalt(10)
   )
 
   // we can now create our user
   User.create(newUser)
       .then(user => {
-        // the new user will be created and redirected to the login page
-        res.redirect('/users/login')
+          // the new user will be created and redirected to the login page
+          res.redirect('/users/login')
       })
-      .catch(error =>{
-        console.log('error')
+      .catch(error => {
+          console.log('error')
 
-        res.send('something went wrong')
+          res.send('something went wrong')
       })
-
 })
+
+
 
 // GET -> login/users/login
 router.get('/login', (req, res) => {
@@ -64,7 +63,45 @@ router.get('/login', (req, res) => {
 
   res.render('users/login', {username, loggedIn, userId})
 })
+// Post -> Login
+router.post('/login', async(req, res) =>{
+ // const { username, loggedIn, userId } = req.session
 
+ // we can pull our credentials from the req.body
+ const { username, password } = req.body
+
+ // search the db for our user
+ // since our usernames are unique, we can use that 
+ User.findOne({ username })
+    .then(async (user) => {
+      // if the user exists
+      if (user){
+        // we compare the password they put in with the one we 
+        // have stored can easily be done with bcrypt
+        // will send either a truthy or falsey value
+        const result = await bcrypt.compare(password, user.password)
+
+        if (result){
+          // if the pws match -> log them in and create the session
+          req.session.username = username
+          req.session.loggedIn = true
+          req.session.userId = user.id
+
+          // once we're loggen in, redirect to the home page
+          res.redirect('/')
+        } else{
+          res.send('something went wrong - no pw match') 
+        }
+      } else{
+        res.send('something went wrong - no user with that name')
+      }
+    })
+    .catch(error => {
+      console.log('error')
+
+      res.send('something went wrong')
+   })
+})
 
 // GET -> logout
 router.get('/logout', (req, res) => {
